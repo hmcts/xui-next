e9e63bfb2e90fe699bba1d2dfb72e4c47f554e29
Stephen Buckley <58293351+oimediaDev@users.noreply.github.com>
2023-06-14 10:03:29 +0100

    Add nestjs server for proxy api -- Basic JWT works on api/auth/login

---
+++ apps/api/src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+//TODO Replace with database if needed
+@Injectable()
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'Demo',
+            password: 'demo',
+        },
+        {
+            userId: 2,
+            username: 'User',
+            password: 'guess',
+        },
+        {
+            userId: 2,
+            username: 'Admin',
+            password: 'guess',
+        },
+    ];
+
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
